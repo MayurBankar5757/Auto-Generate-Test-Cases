b4c00553639b3794ea816e30e9fa583e7903c01b
MayurBankar5757 <mayurbankar272@gmail.com>
2024-06-01 14:21:35 +0530

    First commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+
+            <div className='text-footer'>
+                Developed By Mayur
+
+            </div>
+            <div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
